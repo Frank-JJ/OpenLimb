44b0140c6d366d3dc43890b9e65822f21ee40c8e
Frank-JJ <fjens20@student.sdu.dk>
2024-12-08 01:02:33 +0100

    Added comments to the gaitMachine code.

--- flexControl/gaitMachine.cpp
+++ flexControl/gaitMachine.cpp
@@ -158,8 +158,12 @@ GaitStruct BackCrawl = {
   .gait_amp=1
 };
 
+// Here we define the list of four gaits the joystick can change between
+// In order they are FORWARDS, BACKWARDS, LEFT, RIGHT on the physical joystick
+// As defined in bluetoothHandler.h: ArrowButtons{None, Forwards, Backwards, Left, Right};
 GaitVector movementGaits = {Crawler, TailPush, BackJump, BackCrawl};
 
+// Here we define the motor and limb configurations
 MotorConfigVector motorConfigVector = {
   {BodySide::Left, ServoSide::Left},
   {BodySide::Center, ServoSide::Left},
@@ -176,10 +180,12 @@ GaitMachine::~GaitMachine()
 
 }
 
+// Here we map the interpolated motor values from the gait to actual servo values
 int GaitMachine::mapToMotorValue(float M_pos){
   return (int)min(MOTOR_MAX_VAL,(M_pos * GAIT_AMP * MOTOR_MAX_VAL));
 }
 
+// Here we interpolate between motor commands to the the motor values for a gait over time
 void GaitMachine::gaitControl(Gait gait, std::chrono::microseconds deltaT){
   static int M1_mapped = 0, M2_mapped = 0, M3_mapped = 0;
   static float T_in_gait = 0.0, T_elapsed = 0.0;
@@ -195,44 +201,56 @@ void GaitMachine::gaitControl(Gait gait, std::chrono::microseconds deltaT){
     T_in_gait -= GAIT_T*GAIT_T_AMP;
   }
 
+  // Store initial motor values
   prev_M_pos[0] = MOTOR_INIT[0];
   prev_M_pos[1] = MOTOR_INIT[1];
   prev_M_pos[2] = MOTOR_INIT[2];
   M_pos[0] = MOTOR_INIT[0];
   M_pos[1] = MOTOR_INIT[1];
   M_pos[2] = MOTOR_INIT[2];
+
+  // Go each motor command in the gait
   for(const auto& cmd : gait){
+    // Get the start and end times of the motor command
     float cmd_start_time = cmd.start * GAIT_T*GAIT_T_AMP, cmd_end_time = (cmd.start + cmd.duration) * GAIT_T*GAIT_T_AMP;
+    // Check if we are currently outside these timepoints, and if so, save the motor command amount as the previous motor command
+    // Since a gait is defined from start to end, this successfully saves the value of the previous motor command
     if (T_in_gait < cmd_start_time || T_in_gait >= cmd_end_time){
       prev_M_pos[cmd.motorID] = cmd.amount;
     }
+    // If we are instead inside the motor command start and end times we should interpolate between the previous motor command and the current commands end value
     if(T_in_gait >= cmd_start_time && T_in_gait < cmd_end_time){
+      // First we get the current time elapsed in the motor command
       float elapsed_time_in_cmd = (T_in_gait - cmd_start_time) / (cmd_end_time - cmd_start_time);
-      
+      // Then a linear interpolation is made to get what the motor value should be for the current time
       M_pos[cmd.motorID] = prev_M_pos[cmd.motorID] + (cmd.amount - prev_M_pos[cmd.motorID]) * elapsed_time_in_cmd;
     }
   }
 
+  // The resulting interpolated values must be modified based on joystick input and to take the servo placement on the limb into consideration
   for (int i = 0; i < motorConfigVector.size(); i++)
   {
+    // If a servo is placed on the right side, it will rotate from 100%->0% instead of the normal 0%->100%
     if (motorConfigVector[i].servoSide == ServoSide::Right)
     {
       M_pos[i] = 1 - M_pos[i];
       // Serial.printf("ServoSide::Right");
     }
+    // The right joystick stick modifies servos placed on limbs on the left and right side of the body
     if (motorConfigVector[i].bodySide == BodySide::Left)
     {
-      M_pos[i] = M_pos[i] * min(1, 2 - MODE_DIR*2);
+      M_pos[i] = M_pos[i] * min(1, 2 - MODE_DIR*2); // If a limb is on the left side, the output value should be decreased as the stick is moved to the right
       // Serial.printf(" BodySide::Left");
     }
     else if(motorConfigVector[i].bodySide == BodySide::Right)
     {
-      M_pos[i] = M_pos[i] * min(1, MODE_DIR*2);
+      M_pos[i] = M_pos[i] * min(1, MODE_DIR*2); // If a limb is on the right side, the output value should be decreased as the stick is moved to the left
       // Serial.printf(" BodySide::Right");
     }
     Serial.printf("\n");
   }
 
+  // Finally map the modified interpolated motor commands to the currect motor max and min values
   M1_mapped = mapToMotorValue(M_pos[0]);
   M2_mapped = mapToMotorValue(M_pos[1]);
   M3_mapped = mapToMotorValue(M_pos[2]);
@@ -245,9 +263,11 @@ void GaitMachine::setup(){
   last_call = tick_start;
 }
 
+// Here we select and modify the current gait
 GaitSelectionInfo GaitMachine::selectGait(BluetoothOutput btIn)
 {
   // Serial.printf("arrowButtons: %i | leftJoystickUpDown: %f | rightJoystickUpDown: %f | rightJoystickLeftRight: %f | bluetoothHandler: %i\n", btIn.arrowButtons, btIn.leftJoystickUpDown, btIn.rightJoystickUpDown, btIn.rightJoystickLeftRight, btIn.buttons);
+  // If an arrow button on the joystick has been pressed, a gait should be selected, and its parameters set to its defined values from its GaitStruct
   if (btIn.arrowButtons != ArrowButtons::None)
   {
     // Serial.printf("arrowButtons: %i\n", static_cast<int>(btIn.arrowButtons));
@@ -255,16 +275,24 @@ GaitSelectionInfo GaitMachine::selectGait(BluetoothOutput btIn)
     return {selectedGait.initialMotorPositions, selectedGait.gait, selectedGait.gait_time, selectedGait.gait_amp, directionCenter, 1};
   }
 
+  // If on of the X, B, Y, A buttons has been pressed, we should select the gait as with the arrow buttons, but modify its parameters based on the stick inputs
   if (btIn.buttons != TheFourButtonsOnTheFrontOfTheController::None)
   {
     auto selectedGait = movementGaits[static_cast<int>(btIn.buttons)-1];
     // Serial.printf("btIn.buttons: %i\n", static_cast<int>(btIn.buttons)-1);
+    // The left stick UP/DOWN controls the gait amplitude as a percentage of the MOTOR_MAX_VAL, from 0% (DOWN) to 200% (UP)
+    // The right stick UP/DOWN controls the gait duration as a multiplier of the gaits standard duration, from 0% (DOWN) to 200% (UP)
+    // The right stick LEFT/RIGHT controls the gait direction, gradually turning off the left limbs when turned to the RIGHT and the right limbs when turned to the LEFT 
     return {selectedGait.initialMotorPositions, selectedGait.gait, selectedGait.gait_time, btIn.leftJoystickUpDown*2, btIn.rightJoystickLeftRight, (1-btIn.rightJoystickUpDown)*2};
   }
+
+  // If no button is pressed, the motors are set to 0%
   return {{0,0,0}, movementGaits[0].gait, 0, 0, 0.5, 0};
 }
 
+// This runs the actual gait machine code
 std::array<uint8_t, 3> GaitMachine::loop(BluetoothOutput btIn){
+  // First select and modify a gait
   auto [motor_init, gaitSelected, gait_time, gait_amp, direction, gait_time_modifier] = selectGait(btIn);
   // Serial.printf("gaitSelected: %f, gait_time: %f, gait_amp: %f, direction: %f, gait_time_modifier: %f\n", gaitSelected, gait_time, gait_amp, direction, gait_time_modifier);
   MOTOR_INIT = motor_init;
@@ -273,6 +301,7 @@ std::array<uint8_t, 3> GaitMachine::loop(BluetoothOutput btIn){
   GAIT_AMP = gait_amp;
   MODE_DIR = direction;
 
+  // If a gait has been selected then run it
   if (gaitSelected.size() != 0)
   {
 
@@ -291,5 +320,7 @@ std::array<uint8_t, 3> GaitMachine::loop(BluetoothOutput btIn){
       }
     }
   }
+
+  // Output the resulting motor values
   return motor_pos_array;
 }
--- flexControl/gaitMachine.h
+++ flexControl/gaitMachine.h
@@ -11,44 +11,54 @@
 #include "bluetoothHandler.h"
 
 namespace gaits{
+  // Defines order of limbs and thus what order their actuator values should be in when sent to motorControl
   enum Motors {Right=0, Tail=1, Left=2};
 
+  // Defines a single movement command for a motor
   struct motorCMD{
-    Motors motorID;
-    float amount;
-    float start;
-    float duration;
+    Motors motorID; // What motor id corresponding to one of the limbs defined in motors should be commanded
+    float amount; // What percentage of the gait amplitude should it be commanded to
+    float start; // When during a gait duration should the command start as a percentage
+    float duration; // How long should the command take as a percentage of the gait duration
   };
 
+  // The patter for a gait is a vector of motor commands
   typedef std::vector<motorCMD> Gait;
 
-
+  // A gait has a duration and amplitude, as well as initial motor values.
+  // These parameters are defined with a gait using this struct
   struct GaitStruct{
-    std::vector<float> initialMotorPositions = {0,0,0};
-    Gait gait;
-    float gait_time = 2;   //Duration of the gait's repeating pattern (seconds)
-    float gait_amp = 1;
+    std::vector<float> initialMotorPositions = {0,0,0}; // Here we define the initial motor values
+    Gait gait; // Here the gait is inserted
+    float gait_time = 2; // Duration of the gait's repeating pattern (seconds)
+    float gait_amp = 1; // Amplitude of the gait. A percentage of MOTOR_MAX_VAL
   };
 
+  // Gaits can be stored in a vector for easy indexin
   typedef std::vector<GaitStruct> GaitVector;
 
+  // This struct defines the parameters used to define the currently running gait, based on additional parameters
+  // These parameters are set based on either a defined GaitStruct or modified from joystick input
   struct GaitSelectionInfo{
-    std::vector<float> motor_init = {0,0,0};
-    Gait gait;
-    float gait_time = 2;
-    float gait_amp = 1;
-    float direction = 0;
-    float gait_time_modifier = 1;
+    std::vector<float> motor_init = {0,0,0}; // These are the initial values as from GaitStruct
+    Gait gait; // This is the gait pattern
+    float gait_time = 2; // This is the gait time from a GaitStruct
+    float gait_amp = 1; // This is the gait amplitude as from GaitStruct or from the joystick left stick FORWARDS/BACKWARDS
+    float direction = 0; // This is the gait direction, a modifier to the left and right motors as defined by the MotorConfigVector. It is set by the joystick right stick LEFT/RIGHT. In the center both are activated at 100%, when turned all the way to the LEFT or RIGHT, the opposite side is set to 0%.
+    float gait_time_modifier = 1; // This is a multiplier for the gait duration. This is set by joystick right stick FORWARDS/BACKWARDS. When the stick is FORWARDS the value becomes 0, and thus duration becomes zero. When BACKWARDS the value becomes 2, and duration is doubled.
   };
 
+  // Each limb can be defined as being on the left, center, or right side of the robot body
   enum class BodySide {Left=0, Right=1, Center=2};
+
+  // Each servo can be defined as being on the left or right side of the servo mount for a limb, when looking from the servo mount with the limb foot poiinting away from the viewer
   enum class ServoSide {Left=0, Right=1};
 
+  // The limb and servo sides are encapsulated in a MotorConfig struct, and further in a vector of these
   struct MotorConfig{
     BodySide bodySide;
     ServoSide servoSide;
   };
-
   typedef std::vector<MotorConfig> MotorConfigVector;  
 }
 
@@ -71,8 +81,8 @@ class GaitMachine
     std::array<int8_t, 3> running_commands = {-1,-1,-1};
 
     std::array<uint8_t, 3> program_end_motorpos = {0,0,0};
-    std::array<float, 3> M_pos = {0,0,0};
-    std::array<float, 3> prev_M_pos = {0,0,0};
+    std::array<float, 3> prev_M_pos = {0,0,0}; // When running a gait, we do linear interpolation in each motor command. For this we use the previous commands amount value stored in this variable. If there is no previous command we use the initial motor values
+    std::array<float, 3> M_pos = {0,0,0};  // We store the current linearly interpolated motor values here
 
     std::vector<float> MOTOR_INIT = {0,0,0};
     float GAIT_T = 2;   // Duration of the gait's repeating pattern (seconds).
